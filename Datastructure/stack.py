d5cecd8405d81d13795c57b06a79908e0c0393d0
paleumm <zdotqdot@gmail.com>
2022-08-20 08:57:35 +0700

    Create stack.py

---
+++ Datastructure/stack.py
@@ -0,0 +1,36 @@
+class Stack():
+    def __init__(self, max=None, ls=None) -> None:
+        if list is None:
+            self.items = []
+        else:
+            self.items = ls
+        self.limit = max
+
+    def __str__(self):
+        return str(self.items)
+
+    def __getitem__(self, i:int):
+        return self.items[i]
+
+    def __len__(self):
+        return len(self.items)
+
+    def push(self, x):
+        if not self.isFull():  
+            self.items.append(x)
+
+    def pop(self, index=-1):
+        if not self.isEmpty():
+            return self.items.pop(index)
+        else:
+            print("Stack is empty, no element pop")
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.items[-1]
+
+    def isEmpty(self):
+        return self.__len__() == 0
+    
+    def isFull(self):
+        return self.__len__() == self.limit
