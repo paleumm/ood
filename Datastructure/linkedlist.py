d0f52a65d74c1056707fdd26170df56927d094ea
paleumm <zdotqdot@gmail.com>
2022-08-30 21:51:56 +0700

    Update linkedlist.py

--- Datastructure/linkedlist.py
+++ Datastructure/linkedlist.py
@@ -1,137 +1,60 @@
 class Node:
-    def __init__(self, data, next:Node = None):
+    def __init__(self, data, next):
         self.data = data
         self.next = next
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f'{self.data}'
 
 class LinkedList:
-    def __init__(self, head=None):
-        if head is None:
-            self.head = self.tail = None
-            self.length = 0
-        else:
-            self.head = head
-            temp = self.head
-            self.length = 1
-            while temp.next is not None:
-                temp = temp.next
-                self.length += 1
-            self.tail = temp
-
-    def isEmpty(self):
-        return self.head is None
-
-    def append(self, node:Node):
-        if self.isEmpty():
-            self.head = self.tail = node
-        else:
-            self.tail.next = node
-            self.tail = node
-
-        self.length += 1
+    def __init__(self):
+        self.head = Node(None, None)
+        self.tail = Node(None, None)
+        self.head.next = self.tail
+        self.length = 0
 
     def __str__(self) -> str:
-        if self.isEmpty():
-            return "Empty"
-        cur, s = self.head, f'{self.head.data} '
-        while cur.next is not None:
-            s += f'{cur.next.data} '
-            cur = cur.next
+        current, s = self.head, []
+        while current.next is not self.tail:
+            s.append(str(current.next.data))
+            current = current.next
+        
+        s = ' '.join(s)
         return s
 
-    def index(self, x):
-        if self.isEmpty():
-            return -1
-        
-        cur = self.head
-        idx = 0
-        while cur is not None:
-            if cur.data == x:
-                return idx
-            cur = cur.next
-            idx += 1
-        return -1
-
-    def search(self, x)->bool:
-        if self.isEmpty():
-            return False
-        
-        cur = self.head
-        while cur is not None:
-            if cur.data == x:
-                return True
-            cur = cur.next
-        return False
-    
     def __len__(self):
         return self.length
 
-    def pop(self, idx):
-        if idx >= self.length:
-            return None
+    def isEmpty(self):
+        return self.head.next is None
 
-        if idx == 0: # remove head
-            self.head = self.head.next
-
-        else:
-            pass
-        self.length -= 1
-
-    def insert(self, x:Node, idx=None):
-        if idx >= self.length:
-            return None
-
-        if idx is None or idx == self.length - 1:
-            self.append(x)
-        else:
-            if idx == 0:
-                x.next = self.head
-                self.head = x
-            else:
-                i = 0
-                cur = self.head
-                while cur is not None and i < idx:
-                    if i + 1 == idx:
-                        x.next = cur.next
-                        cur.next = x
-
-                    cur = cur.next
-                    i += 1
-        self.length += 1           
-
-    def __getitem__(self, i:int):
-        if self.isEmpty():
-            return None
-        
+    def append(self, data):
+        node = Node(str(data), self.tail)
         cur = self.head
+        # print(f'append {cur}')
+        while cur.next is not self.tail:
+            cur = cur.next
+        cur.next = node
+        # print(f'len = {l}')
+        self.length += 1
+
+    def insert(self, index, data):
+        if index == self.length:
+            self.append(data)
+            return
+
+        node = Node(str(data), None)
         idx = 0
-        while cur is not None and idx <= i:
-            if idx == i:
-                return cur.data
+        cur = self.head
+        # print(cur.next)
+        
+        while cur.next is not self.tail:
+            # print(f'{cur} {cur.next} {idx}')
+            if idx == index:
+                node.next = cur.next
+                cur.next = node
+                break
+                
             cur = cur.next
             idx += 1
-        return None
-
-
-l = LinkedList()
-# print(l)
-n1 = Node(1)
-n2 = Node(2)
-n3 = Node(3)
-l.append(n1)
-l.append(n2)
-l.append(n3)
-n1 = Node(1)
-n2 = Node(9)
-n3 = Node(3)
-l.append(n1)
-l.append(n2)
-l.append(n3)
-print(l)
-print(len(l))
-print(l[4])
-l.insert(Node(5), 9)
-print(l)
-print(len(l))
+        self.length += 1
