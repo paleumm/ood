80228367b3d7a0aad20f20b2b67ccfbba837192e
paleumm <zdotqdot@gmail.com>
2022-10-21 00:26:05 +0700

    Create simple-avl.py

---
+++ chapter8/simple-avl.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def __str__(self):
+        return str(self.data)
+
+class AVL:
+
+    def __init__(self) -> None:
+        pass
+
+    def printTree(self, node, level = 0):
+        if node != None:
+            self.printTree(node.right, level + 1)
+            print('     ' * level, node)
+            self.printTree(node.left, level + 1)
+
+    def insert(self, data):
+        pass
