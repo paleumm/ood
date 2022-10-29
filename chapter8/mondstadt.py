da910d91176f831fbe93bfdc8bd3a3e2da53bd21
Permpoon Boonyarit <zdotqdot@gmail.com>
2022-10-29 14:44:21 +0700

    Update mondstadt.py

--- chapter8/mondstadt.py
+++ chapter8/mondstadt.py
@@ -54,11 +54,18 @@ class Tree:
 
         return q
 
-    def findPower(self, idx) -> int:
-        power = 0
+    def findPower(self, idx1, idx2):
+        k1 = T.inorder(T.bfs(idx1),0)
+        k2 = T.inorder(T.bfs(idx2),0)
 
+        if k1 > k2:
+            print(f'{idx1}>{idx2}')
+        elif k1 < k2:
+            print(f'{idx1}<{idx2}')
+        else:
+            print(f'{idx1}={idx2}')
 
-        return power
+        
 
     def bfs(self, idx) -> Node:
         l = []
@@ -127,43 +134,11 @@ inp = input('Enter Input : ').split("/")
 p = [int(i) for i in inp[0].split()]
 pair = [i for i in inp[1].split(",")]
 
-print(p)
-print(pair)
-
 print(sum(p))
 
 for i in p:
     T.insert(i)
-    
-printTree90(T.root)
-# inp = input("Enter Input : ").split("/")
-# fir, sec, ans = [int(i) for i in inp[0].split()], inp[1].split(","), 0
-# ans = sum(fir)
-# print(ans)
 
-# for i in sec:
-#     a, b = int(i.split(" ")[0]), int(i.split(" ")[1])
-#     if 2*a+1 >= len(fir):
-#         a1 = 0
-#     else:
-#         a1 = fir[2*a+1]
-#     if 2*a+2 >= len(fir):
-#         a2 = 0
-#     else:
-#         a2 = fir[2*a+2]
-#     if 2*b+1 >= len(fir):
-#         b1 = 0
-#     else:
-#         b1 = fir[2*b+1]
-#     if 2*b+2 >= len(fir):
-#         b2 = 0
-#     else:
-#         b2 = fir[2*b+2]
-#     sum_a = fir[a] + a1 + a2
-#     sum_b = fir[b] + b1 + b2
-#     if sum_a > sum_b:
-#         print(f'{a}>{b}')
-#     elif sum_a < sum_b:
-#         print(f'{a}<{b}')
-#     else:
-#         print(f'{a}={b}')
+for pp in pair:
+    K = pp.split()
+    T.findPower(int(K[0]),int(K[1]))
