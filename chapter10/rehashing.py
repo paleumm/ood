564d6cb6e54de5ef536593af1fae98bec8ec3409
paleumm <zdotqdot@gmail.com>
2022-11-22 13:54:50 +0700

    Update rehashing.py

--- chapter10/rehashing.py
+++ chapter10/rehashing.py
@@ -1,82 +1,100 @@
-class Data:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-
-    def __str__(self):
-        return "({0}, {1})".format(self.key, self.value)
-
-class hash:
-    def __init__(self, tableSize: int, maxCollision: int) -> None:
+class Hash:
+    def __init__(self, tableSize: int, maxCollision: int, threshold: int) -> None:
         self.table = [None]*tableSize
         self.maxCollision = maxCollision
         self.tableSize = tableSize
-        self.num = 0
-        self.h = 0
+        self.threshold = threshold
+        self.inserted = []
+        self.collide = 0
+        self.hashed = 0
 
-    def printHashTable(self) -> str:
+        print("Initial Table :")
+        self.printHashTable()
+
+    def printHashTable(self):
         for i in range(1, self.tableSize + 1):
             print(f'#{i}	{self.table[i-1]}')
-        print('---------------------------')
+        print('----------------------------------------')
 
-    def hashing(self, value: Data):
-        idx, data = self.hashFunction(value)
-        # print(data)
-        if self.collided(data, idx):
-            # if self.num == self.maxCollision:
-            idx, data = self.quadProbe(value=value)
-            if idx == -999:
-                self.printHashTable()
+    def insert(self, data):
+        self.hashed = 0
+        self.collide = 0
+        if data not in self.inserted: # print 'Add: data' when inserted new data ony
+            self.inserted.append(data)
+            print(f'Add : {data}')
+        while True:
+            idx = self.hashF(data, int(self.collide))
+            if self.exceedThreshold(): # over threshold
+                print(f'****** Data over threshold - Rehash !!! ******')
+                self.rehash()
                 return
-        self.h += 1
-        self.table[idx] = data
-        self.printHashTable()
-        if self.h >= self.tableSize:
-            print('This table is full !!!!!!')
-            exit(0)
+            if self.collided(idx) and self.collide < self.maxCollision + 1: # collision case
+                if self.collide < self.maxCollision:
+                    print(f'collision number {self.collide+1} at {idx}')
+                
+                self.hashed += 1
+                self.collide += 1
 
-    def collided(self, value: Data, idx):
+                if self.collide == self.maxCollision: # max collision
+                    print('****** Max collision - Rehash !!! ******')
+                    self.rehash()
+                    return
+            else: # case not collide or exceed threshold
+                self.table[idx] = data
+                break
+
+        # print only when we insert new data only (when rehash)
+        if data == self.inserted[-1]:
+            self.printHashTable()
+
+
+
+    def hashF(self, data, i=0) -> int:
+        idx = data % self.tableSize
+        idx = (idx + i*i) % self.tableSize
+        return idx
+
+    def collided(self, idx) -> bool:
         if self.table[idx] is not None:
             return True
         return False  
 
-    def hashFunction(self, value: Data) -> Data:
-        key = value.key
-        sum = 0
-        for ch in key:
-            sum += ord(ch)
-        idx = sum % self.tableSize
-        return idx, value
+    def exceedThreshold(self) -> bool:
+        count = 0
+        for d in self.table:
+            if d is not None:
+                count += 1
+            if count + 1 >= float(self.threshold* self.tableSize / 100):
+                return True
+        return False
 
-    def quadProbe(self, value: Data):
-        idx, data = self.hashFunction(value)
-        for i in range(self.tableSize):
-            temp = (idx + i*i) % self.tableSize
-            if not self.collided(data, temp):
-                self.num = 0
-                return temp, data
-            else:
-                self.num += 1
-                print(f'collision number {self.num} at {temp}')
-            if self.num == self.maxCollision:
-                print('Max of collisionChain')
-                self.num = 0
-                return -999, None
-        return -999, None
+    def nextPrime(self, num) -> int:
+        while True:
+            check = True
+            for i in range(2, int(num/2)+1):
+                if (num % i) == 0:
+                    check = False
+                    break
+            if check:
+                return num
+            num += 1
+
+    def rehash(self):
+        self.tableSize = self.nextPrime(self.tableSize * 2)
+        self.table = [None]*self.tableSize
+        for d in self.inserted:
+            self.insert(d)
 
 
-print(' ***** Fun with hashing *****')
+print(' ***** Rehashing *****')
 inp = input('Enter Input : ').split('/')
-tableSz, maxColl = inp[0].split()
-data = inp[1].split(',')
+tableSz, maxColl, threshold = inp[0].split()
+tableSz = int(tableSz)
+maxColl = int(maxColl)
+threshold = int(threshold)
+data = inp[1].split()
 
-dataIn = []
+h = Hash(tableSz, maxColl, threshold)
 
-for i in data:
-    key, val = i.split()
-    d = Data(key=key, value=val)
-    dataIn.append(d)
-h = hash(int(tableSz), int(maxColl))
-
-for d in dataIn:
-    h.hashing(d)
+for d in data:
+    h.insert(int(d))
